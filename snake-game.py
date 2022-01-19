92e88b2615ea2dcca3ef4f3971e1bc74115ae4ee
JHE4N <93744970+JHE4N@users.noreply.github.com>
2022-01-19 07:53:24 -0300

    Atualização
    Adicionado funções

--- snake-game.py
+++ snake-game.py
@@ -15,70 +15,73 @@ except:
 largura = 640
 altura = 480
 tamanho = 10
-pos_x = randint(0, (largura - tamanho) /10) *10
-pos_y = randint(0, (altura - tamanho) /10) *10
-x_maca = randint(0, (largura - tamanho) /10) *10
-y_maca = randint(0, (altura - tamanho) /10) *10
-velocidade_x = 0
-velocidade_y = 0
+
 relogio = pygame.time.Clock()
 pygame.display.set_caption('Snake') #Nome do jogo
 fundo = pygame.display.set_mode((largura, altura)) #definindo largura e altura da tela
 
-sair = True
-while sair:
-    for event in pygame.event.get(): #Enquanto acontecer eventos, entra no loop
-        if event.type == pygame.QUIT: #Se apertar no [X] que fecha o jogo
-            sair = False
-        #Eventos para quando andar para qualquer direção
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT: #Se andar pra esquerda
-                velocidade_y = 0
-                velocidade_x = -tamanho   
-            if event.key == pygame.K_RIGHT: #Se andar pra direita
-                velocidade_y = 0
-                velocidade_x = tamanho
-            if event.key == pygame.K_UP: #Se andar pra cima
-                velocidade_x = 0
-                velocidade_y = -tamanho
-            if event.key == pygame.K_DOWN: #Se andar pra baixo
-                velocidade_x = 0
-                velocidade_y = tamanho
-                
-        print(event)  #printar no prompt o que ta acontecendo
-    fundo.fill(branco) #cor de fundo
-    serpente = pygame.draw.rect(fundo, verde, [pos_x, pos_y, tamanho, tamanho]) #desenhar o quadrado (serpente)
-    maca = pygame.draw.circle(fundo, vermelho, (x_maca, y_maca), 5, 5) #desenhar o circulo (maçã)
+def cobra(pos_x_cobra, pos_y_cobra):
+    pygame.draw.rect(fundo, preto, [pos_x_cobra, pos_y_cobra, tamanho, tamanho]) #desenhar o quadrado (serpente)
     
-    pos_x += velocidade_x #andar no eixo x
-    pos_y += velocidade_y #andar no eixo y
-    pygame.display.update() #recarregar a janela
+def maca(pos_x_maca, pos_y_maca):
+        pygame.draw.rect(fundo, vermelho, [pos_x_maca, pos_y_maca, tamanho, tamanho]) #desenhar o quadrado (serpente)
     
-    # pygame.font.init()
-    # pygame.font.SysFont()
-    # pygame.font.size(largura, altura)
-    # pygame.font.Font.metrics(Arial, 'this is text')
-    # pygame.font.Font.render(largura, altura)
+def jogo():
+    #Cobra
+    pos_x_cobra = randint(0, (largura - tamanho) /10) *10 #posição x
+    pos_y_cobra = randint(0, (altura - tamanho) /10) *10 #posição y
     
-    relogio.tick(10)
-    #pygame.draw.circle(fundo, azul, [pos_x, pos_y, tamanho, tamanho])
+    #Maça
+    pos_x_maca = randint(0, (largura - tamanho) /10) *10 #posição x
+    pos_y_maca = randint(0, (altura - tamanho) /10) *10 #posição y
     
-    #Eventos para caso a serpente chegar na borda da tela
-    if pos_x >= largura:
-        pos_x = 1
-    if pos_x < 1:
-        pos_x = 639
-    if pos_y >= altura:
-        pos_y = 1
-    if pos_y < 1:
-        pos_y = 479
-        
-    #pygameif pos_x == x_maca and pos_y == y_maca: #Se a serpente comer a maça
-        #print("pegou a maca")
-        #desenhar o quadrado (serpente)
-    #     tamanho = tamanho + 5 #Serpente ganha tamanho
-    
-        
-        
-#pygame.quit()
-#quit()
+    velocidade_x = 0
+    velocidade_y = 0
+    sair = True
+    while sair:
+        for event in pygame.event.get(): #Enquanto acontecer eventos, entra no loop
+            if event.type == pygame.QUIT: #Se apertar no [X] que fecha o jogo
+                sair = False
+            #Eventos para quando andar para qualquer direção
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT and velocidade_x != tamanho: #Se andar pra esquerda
+                    velocidade_y = 0
+                    velocidade_x = -tamanho   
+                if event.key == pygame.K_RIGHT and velocidade_x != tamanho: #Se andar pra direita
+                    velocidade_y = 0
+                    velocidade_x = tamanho
+                if event.key == pygame.K_UP and velocidade_y != tamanho: #Se andar pra cima
+                    velocidade_x = 0
+                    velocidade_y = -tamanho
+                if event.key == pygame.K_DOWN and velocidade_y != tamanho: #Se andar pra baixo
+                    velocidade_x = 0
+                    velocidade_y = tamanho
+                    
+            print(event)  #printar no prompt o que ta acontecendo
+        fundo.fill(branco) #cor de fundo      
+        pos_x_cobra += velocidade_x #andar no eixo x
+        pos_y_cobra += velocidade_y #andar no eixo y
+        cobra(pos_x_cobra, pos_y_cobra)
+        maca(pos_x_maca, pos_y_maca)
+        pygame.display.update() #recarregar a janela
+        relogio.tick(10)
+
+        #Eventos para caso a serpente chegar na borda da tela
+        if pos_x_cobra >= largura:
+            pos_x_cobra = 1 #game over
+        if pos_x_cobra < 1:
+            pos_x_cobra = 639 #game over
+        if pos_y_cobra >= altura:
+            pos_y_cobra = 1 #game over
+        if pos_y_cobra < 1:
+            pos_y_cobra = 479 #game over
+            
+        #pygameif pos_x == x_maca and pos_y == y_maca: #Se a serpente comer a maça
+            #print("pegou a maca")
+            #desenhar o quadrado (serpente)
+        #     tamanho = tamanho + 5 #Serpente ganha tamanho
+            
+            
+    #pygame.quit()
+    #quit()
+jogo()
